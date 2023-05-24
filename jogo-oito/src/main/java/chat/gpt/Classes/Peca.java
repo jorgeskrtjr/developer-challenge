acade7100e5138d7a49ac775d129de44e62068d9
Jorge jr <jorgesilvajunior10@gmail.com>
2023-05-24 16:18:45 -0300

    add: Peca

---
+++ jogo-oito/src/main/java/chat/gpt/Classes/Peca.java
@@ -0,0 +1,35 @@
+package org.example.classes;
+
+public class Peca {
+    private int linha;
+    private int coluna;
+    private int numero;
+
+    public Peca(int numero, int linha, int coluna) {
+        this.numero = numero;
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public void setLinha(int value){
+        this.linha = value;
+    }
+
+    public int getLinha(){
+        return this.linha;
+    }
+
+    public void setColuna(int value){
+        this.coluna = value;
+    }
+
+    public int getColuna(){
+        return this.coluna;
+    }
+
+    public void setNumero(int value){
+        this.numero = value;
+    }
+
+    public int getnumero(){return this.numero;}
+}
